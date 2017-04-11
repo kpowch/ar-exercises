c9e92e64a8250bfde3e288f6192877e47f19f82c
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-11 21:34:12 +0000

    calcaultes total and average revenue of all stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,13 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# calculates total revenue
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total revenue: #{@total_revenue}"
+
+# calculates average annual_revenue
+@average_revenue = Store.average(:annual_revenue)
+puts "Average revenue: #{@average_revenue}"
+
+# outputs number of stores that generate $1mil+
+puts Store.where("annual_revenue >= 1000000").count
