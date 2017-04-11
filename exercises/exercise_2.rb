263778b1a56b7e68f39c79fc0880c5ddc9763594
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-11 21:06:22 +0000

    adds comments

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,9 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
+# assigns store 1 and 2 to variables
 @store1 = Store.find(1)
-# puts @store1.name
 @store2 = Store.find(2)
 
+# changes name of first store
 @store1.name = "Steveston"
-# puts @store1.name
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,6 +5,8 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
+# deletes 3rd store
 @store3 = Store.find(3).destroy
 
+# outputs new number of stores
 puts Store.count
