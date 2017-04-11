4317a6eac68b82596c14fb0c97b88810f144e4d0
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-11 23:13:29 +0000

    adds validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,10 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :store, presence: true
+
+  validates :first_name, :last_name, presence: true
+
+  validates :hourly_rate, numericality: { only_integer: true }
+
+  validates_inclusion_of :hourly_rate, in:40..200
 end
