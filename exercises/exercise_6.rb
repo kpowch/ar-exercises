577c6d08c7f73e537a406e4120b91d2ba7658120
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-11 21:50:08 +0000

    adds employees to store1 and 2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,35 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+# creates some employees and adds them to a store
+@store1.employees.create(
+  first_name: "Bob",
+  last_name: "Wilson",
+  hourly_rate: "600"
+  )
+
+@store1.employees.create(
+  first_name: "Mary",
+  last_name: "Man",
+  hourly_rate: "60"
+  )
+
+@store1.employees.create(
+  first_name: "Sallay",
+  last_name: "Simmons",
+  hourly_rate: "6000"
+  )
+
+@store2.employees.create(
+  first_name: "Micky",
+  last_name: "Rogh",
+  hourly_rate: "6"
+  )
+
+@store2.employees.create(
+  first_name: "Mack",
+  last_name: "Michales",
+  hourly_rate: "0.60"
+  )
+
+puts Employee.count
