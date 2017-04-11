55b585c1d794822b7a38f89edbf486e2b3cafbd2
Ksenia Owchar <ksenia.owchar@gmail.com>
2017-04-11 21:26:44 +0000

    adds more stores and filters based on mens/womens apparel

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,40 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# creates 3 more stores
+Store.create({
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+  })
+
+Store.create({
+  name: "Whistler",
+  annual_revenue: 19000000,
+  mens_apparel: true,
+  womens_apparel: false
+  })
+
+Store.create({
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+  })
+
+# returns array of stores that carry mens apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# outputs store name and annual_revenue
+@mens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
+
+# returns array of stores that carry womens apparel and has annual_revenue < 1M
+@womens_stores = Store.where("womens_apparel = true AND annual_revenue < 1000000")
+
+# outputs store name and annual_revenue
+@womens_stores.each do |store|
+  puts "#{store.name}, #{store.annual_revenue}"
+end
